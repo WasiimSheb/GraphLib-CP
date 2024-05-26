a8d1f50fc7567cea2bdb135abf34a6625347a1c6
WasiimSheb <123734906+WasiimSheb@users.noreply.github.com>
2024-05-26 13:32:19 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -5,42 +5,68 @@
 #include <vector>
 using namespace std;
 
-namespace ariel{
-    class Graph{
-        private:
-            vector<vector<int>> adjacencymatrix;
-            bool issquared(const Graph& other) const;
-            bool containsGraph(const Graph& other) const;
-            int numofedges(const Graph& other) const;
+namespace ariel
+{
+    class Graph
+    {
+    private:
+        vector<vector<int>> adjacencymatrix;
+        bool issquared(const Graph &other) const;
+        bool containsGraph(const Graph &other) const;
+        int numofedges(const Graph &other) const;
 
-        public:
+    public:
+        Graph();
+        Graph(int);
+        ~Graph();
+        void printGraph();
+        void loadGraph(vector<vector<int>> adjmat);
+        const vector<vector<int>> &getAdjacencyMatrix() const;
+        size_t getNumVertices() const;
+        Graph &operator++();
+        Graph &operator+();
+        Graph &operator-();
+        Graph &operator++(int);
+        Graph &operator--();
+        Graph &operator--(int);
+        Graph &operator-=(const Graph &mat);
+        Graph &operator+=(const Graph &mat);
+        Graph &operator*=(int scalar);
+        Graph &operator/=(int scalar);
+        Graph operator+(const Graph &mat);
+        Graph operator-(const Graph &mat);
+        Graph operator*(const Graph &mat);
+        Graph &operator*(int);
+        Graph &operator/(int);
+        bool operator==(const Graph &mat) const;
+        bool operator>=(const Graph &mat) const;
+        bool operator<=(const Graph &mat) const;
+        bool operator!=(const Graph &mat) const;
+        bool operator<(const Graph &mat) const;
+        bool operator>(const Graph &mat) const;
 
-            Graph();
-            Graph(int);
-            ~Graph();
-            void printGraph();
-            void loadGraph(vector<vector<int>> adjmat);
-            const vector<vector<int>> &getAdjacencyMatrix() const;
-            size_t getNumVertices() const;
-            Graph& operator++();
-            Graph& operator+();
-            Graph& operator-();
-            Graph& operator++(int);
-            Graph& operator--();
-            Graph& operator--(int);
-            Graph& operator-=(const Graph& mat);
-            Graph& operator+=(const Graph& mat);
-            Graph operator+(const Graph& mat);
-            Graph operator-(const Graph& mat);
-            Graph operator*(const Graph& mat);
-            Graph& operator*(int);
-            Graph& operator/(int);
-            bool operator==(const Graph& mat) const;
-            bool operator>=(const Graph& mat) const;
-            bool operator<=(const Graph& mat) const;
-            bool operator!=(const Graph& mat) const;
-            bool operator<(const Graph& mat) const;
-            bool operator>(const Graph& mat) const;
+        friend ostream &operator<<(ostream &os, const Graph &graph)
+        {
+            const vector<vector<int>> &matrix = graph.getAdjacencyMatrix();
+            for (size_t i = 0; i < matrix.size(); ++i)
+            {
+                os << "[";
+                for (size_t j = 0; j < matrix[i].size(); ++j)
+                {
+                    os << matrix[i][j];
+                    if (j != matrix[i].size() - 1)
+                    {
+                        os << ", ";
+                    }
+                }
+                os << "]";
+                if (i != matrix.size() - 1)
+                {
+                    os << ", ";
+                }
+            }
+            return os;
+        }
     };
 }
 #endif // GRAPH_HPP
